b6601fbdd6bf773173a54fa291e3917e9a1507f5
JSLucena <joca.lucena@gmail.com>
2023-08-11 19:56:50 -0300

    changing stuff

--- Scripts/game_resources.gd
+++ Scripts/game_resources.gd
@@ -1,19 +1,18 @@
 extends Node
 
 const BULDING_COUNT_MAX = 5
-
+var SERVER_RATE = 1
+var building_preload = preload("res://Scenes/Sprite2Ds/building.tscn")
 enum building_index{
 	VACANT = 0,
-	CITY_HALL = 1
+	CITY_HALL = 1,
+	FORESTER = 2
 }
 enum resource_index{
 	GOLD = 0,
 	WOOD = 1
 }
-var building_sprite_dictionary = {
-	0 : preload("res://Scenes/Sprite2Ds/Vacant_Space.tscn"),
-	1 : preload("res://Scenes/Sprite2Ds/City_Hall.tscn")
-}
+
 var resource_sprite_dictionary = {
 	0 : preload("res://Scenes/Sprite2Ds/Gold.tscn"),
 	1 : preload("res://Scenes/Sprite2Ds/Wood.tscn")
@@ -32,7 +31,7 @@ class Building:
 	var sprite_index : int
 	var building_menu : Node2D
 	var current_level : int
-	var level_time : Dictionary
+	var level_resources : Dictionary # key is level, then Gold, Wood, etc ....., last  is time
 	var effect : Dictionary
 	
 	
@@ -66,23 +65,9 @@ func init_resources(): # initializes available resources
 	print(available_resources)
 
 func init_buildings():
-	var new_res = Building.new()
-	new_res.building_name = "Vacant"
-	new_res.description = "This is a vacant lot, maybe you should build something?"
-	new_res.sprite_index  = 0
-	#new_res.building_menu = ...
-	new_res.current_level = 0
-	available_buildings.append(new_res)
-	
-	new_res.building_name = "City Hall"
-	new_res.description = "This is the City Hall, where you can control your population and taxes"
-	new_res.sprite_index  = 1
-	#new_res.building_menu = ...
-	new_res.current_level = 1
-	#new_res.level_time = {}
-	new_res.effect[1] = {"Max_Population":[100,"a"], "Gold_Production":[1.01,"m"]}
-	
-	available_buildings.append(new_res)
+	init_building(build_dict["Vacant"],"Vacant")
+	init_building(build_dict["City Hall"],"City Hall")
+	init_building(build_dict["Forester"],"Forester")
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -94,3 +79,52 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func init_building(bld_info,bld_name):
+	var new_res = Building.new()
+	new_res.building_name = bld_name
+	new_res.description = bld_info[0]
+	new_res.sprite_index  = bld_info[1]
+	#new_res.building_menu = bld_info[2]
+	new_res.current_level = bld_info[3]
+	new_res.level_resources = bld_info[4]
+	new_res.effect = bld_info[5]
+	available_buildings.append(new_res)
+
+var build_dict = {
+	"Vacant" : ["This is a vacant lot, maybe you should build something?", 0, "...", 0, {1 : [0,0,0]}, {0 : 0}],
+	
+	"City Hall" : ["This is the City Hall, where you can control your population and taxes", 
+	1, 
+	"...", 
+	0,
+	{
+		1 : [50,50,60],
+		2 : [80,60,240],
+		3 : [100,100,600]
+	},
+	{
+		1 : {"Max_Population":[100,"a"], "Gold_Production":[1.01,"m"]},
+		2 : {"Max_Population":[150,"a"], "Gold_Production":[1.03,"m"]},
+		3 : {"Max_Population":[220,"a"], "Gold_Production":[1.05,"m"]}
+	}
+	],
+	
+	"Forester" : ["This is the Forester, where you can put citizens to chopp wood", 
+	2, 
+	"...", 
+	0,
+	{
+		1 : [10,0,12],
+		2 : [35,5,24],
+		3 : [50,15,40]
+	},
+	{
+		1 : {"Max_Lumberjacks":[5,"i"]},
+		2 : {"Max_Lumberjacks":[15,"i"]},
+		3 : {"Max_Lumberjacks":[30,"i"]}
+	}
+	]
+	
+	}
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -7,37 +7,45 @@ var modded
 
 
 func apply_modifier(key, stat, building):
-	print(building.effect[building.current_level])
+#	print(building.effect[building.current_level])
 	if building.effect[building.current_level][key][1] == "a":
 		return stat + building.effect[building.current_level][key][0]
 	else:
 		return stat * building.effect[building.current_level][key][0]
 func _ready():
-	this_city.city_name = "test"
-	this_city.location = Vector2(0,0)
-	this_city.population = 50
-	this_city.max_population = 50
-	for thing in gameRes.available_resources:
-		print(thing.res_name)
-		this_city.resource_list[thing.res_name] = thing
-	for i in range(0,5):
-		var name = "space" + str(i)
-		this_city.building_list[i] = [gameRes.available_buildings[gameRes.building_index.VACANT],get_node(name)]
-	
-	this_city.resource_list["Gold"].production = this_city.population * 0.1
-	#creating city hall
-	var new_building = gameRes.building_sprite_dictionary[gameRes.building_index.CITY_HALL].instantiate()
-	new_building.position = $space0.position
-	new_building.name = $space0.name
-	$space0.queue_free()
-	add_child(new_building)
-	this_city.building_list[0] = [gameRes.available_buildings[gameRes.building_index.CITY_HALL],get_node("space0")]
-	modded = this_city
-	print(this_city.building_list[0][0].building_name)
-	modded.max_population = apply_modifier("Max_Population",modded.max_population, this_city.building_list[0][0])
-	print(modded.max_population)
-	modded.resource_list["Gold"].production = apply_modifier("Gold_Production",modded.resource_list["Gold"].production, this_city.building_list[0][0])
-	
+	if PlayerControl.city_list.is_empty():
+		this_city.city_name = "test"
+		this_city.location = Vector2(0,0)
+		this_city.population = 50
+		this_city.max_population = 50
+		for thing in gameRes.available_resources:
+			print(thing.res_name)
+			this_city.resource_list[thing.res_name] = thing
+		for i in range(0,5):
+			var name = "space" + str(i)
+			this_city.building_list[i] = [gameRes.available_buildings[gameRes.building_index.VACANT],get_node(name)]
+		
+		this_city.resource_list["Gold"].production = this_city.population * 0.1
+		#creating city hall
+		var new_building = gameRes.building_preload.instantiate()
+		new_building.type = "City Hall"
+		new_building.modulate = Color(1.0, 1.0, 0, 1.0)
+		new_building.scale = Vector2(2,2)
+		
+		new_building.position = $space0.position
+		new_building.name = $space0.name
+		$space0.queue_free()
+		add_child(new_building)
+
+		this_city.building_list[0] = [gameRes.available_buildings[gameRes.building_index.CITY_HALL],get_node("space0")]
+		modded = this_city
+		print(this_city.building_list[0][0].building_name)
+		modded.max_population = apply_modifier("Max_Population",modded.max_population, this_city.building_list[0][0])
+		print(modded.max_population)
+		modded.resource_list["Gold"].production = apply_modifier("Gold_Production",modded.resource_list["Gold"].production, this_city.building_list[0][0])
+		PlayerControl.city_list.append(this_city)
+	else :
+		pass
 	
 
 
